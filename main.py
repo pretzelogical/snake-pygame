f34c60f0afa7fa22feb4a6ab8bc13d298b369539
pretzelogical <yingyangyo266@gmail.com>
2023-10-14 18:54:10 -0500

    block moving onscreen

--- main.py
+++ main.py
@@ -19,23 +19,36 @@ class Snake(pygame.Rect):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.color = COL_BLACK
-        self.pos = Vector2(512, 512)
+        self.pos = Vector2(16, 16)
+        self.x = self.pos.x * GRID_SIZE
+        self.y = self.pos.y * GRID_SIZE
 
-    def move(self, keys, gameField):
+    def move(self, heading, gameField):
         """ Processes movement """
-        keyMap = {
-            pygame.K_LEFT: (-1, 0),
-            pygame.K_RIGHT: (1, 0),
-            pygame.K_UP: (0, -1),
-            pygame.K_DOWN: (0, 1)
+        headingMap = {
+            pygame.K_LEFT: Vector2(-1, 0),
+            pygame.K_RIGHT: Vector2(1, 0),
+            pygame.K_UP: Vector2(0, -1),
+            pygame.K_DOWN: Vector2(0, 1)
                    }
-        
+        if heading in headingMap:
+            self.pos += headingMap[heading]
+        self.x = self.pos.x * GRID_SIZE
+        self.y = self.pos.y * GRID_SIZE
+        if TESTING:
+            print(pygame.key.name(heading))
+            print(self.pos)
+
+    def perimCheck(self, gameField):
+        """ Checks if snake is colliding with another block"""
+        raise NotImplementedError("perimCheck not implemented")
 
 
 class Food(pygame.Rect):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.color = COL_RED
+        self.pos = Vector2(10, 13)
 
 
 def createGameField():
@@ -76,6 +89,7 @@ def main():
     food = Food(0, 0, GRID_SIZE, GRID_SIZE)
     blocks = [snake, food]
     running = True
+    heading = pygame.K_UP
 
     while running:
         events = pygame.event.get()
@@ -85,7 +99,12 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if TESTING:
                     print(pygame.key.name(event.key))
+                heading = event.key
 
+        # Game logic
+        snake.move(heading, gameField)
+
+        # Drawing logic
         screen.fill((255, 255, 255))
         drawGameGrid(screen)
         drawBlocks(screen, blocks)
@@ -94,7 +113,7 @@ def main():
             testDraw(screen, frameCount)
 
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(2)
     pygame.quit()
 
 
