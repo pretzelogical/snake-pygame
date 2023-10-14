f22a93d380c994aef0afc391392cfa306842942e
pretzelogical <yingyangyo266@gmail.com>
2023-10-14 18:29:38 -0500

    add testing helpers

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python3
 import pygame
+from pygame.math import Vector2
+from sys import argv
+pygame.init()
 
 SCREEN_SIZE = 1024
 # Pixel size of grid squares
 GRID_SIZE = 32
+TESTING = "test" in argv or "Test" in argv
 # Colors
 COL_BLACK = pygame.Color("black")
 COL_RED = pygame.Color("red")
+# Font
+FONT = pygame.font.SysFont("Cantarell", 32)
+
 
 class Snake(pygame.Rect):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.color = COL_BLACK
+        self.pos = Vector2(512, 512)
+
+    def move(self, keys, gameField):
+        """ Processes movement """
+        keyMap = {
+            pygame.K_LEFT: (-1, 0),
+            pygame.K_RIGHT: (1, 0),
+            pygame.K_UP: (0, -1),
+            pygame.K_DOWN: (0, 1)
+                   }
         
+
+
 class Food(pygame.Rect):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.color = COL_RED
+
 
 def createGameField():
     """ Returns a cols x rows list"""
@@ -29,15 +49,27 @@ def drawGameGrid(screen):
         pygame.draw.line(screen, COL_BLACK, (i, 0), (i, SCREEN_SIZE))
         pygame.draw.line(screen, COL_BLACK, (0, i), (SCREEN_SIZE, i))
 
+
 def drawBlocks(screen, blocks):
     """ Draws blocks """
     for block in blocks:
         pygame.draw.rect(screen, block.color, block)
 
+
+def testDraw(screen, frames):
+    """ Draws testing info """
+    frameText = FONT.render(f"{frames}", True, COL_RED)
+    frameTextRect = frameText.get_rect()
+    frameTextRect.center = (SCREEN_SIZE // 2, SCREEN_SIZE // 4)
+    screen.blit(frameText, frameTextRect)
+
+
 def main():
-    pygame.init()
     screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
     pygame.display.set_caption("Snake")
+    if TESTING:
+        print("Testing mode")
+        frameCount = 0
     gameField = createGameField()
     clock = pygame.time.Clock()
     snake = Snake(GRID_SIZE, GRID_SIZE, GRID_SIZE, GRID_SIZE)
@@ -46,17 +78,24 @@ def main():
     running = True
 
     while running:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 running = False
-    
+            if event.type == pygame.KEYDOWN:
+                if TESTING:
+                    print(pygame.key.name(event.key))
 
         screen.fill((255, 255, 255))
         drawGameGrid(screen)
         drawBlocks(screen, blocks)
-        pygame.display.flip()
+        if TESTING:
+            frameCount += 1
+            testDraw(screen, frameCount)
 
+        pygame.display.flip()
         clock.tick(60)
     pygame.quit()
+
 
 main()
